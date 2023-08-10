861732f187c737d2d2d2fabb3f16be956d80fa56
ghaitsa02 <bangghaitsa@gmail.com>
2023-08-10 09:23:26 +0700

    stateLessFull

---
+++ lib/listView.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const aplikasi());
+}
+
+class aplikasi extends StatelessWidget {
+  const aplikasi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -15,19 +15,40 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('My Apps'),
         ),
-        backgroundColor: Colors.blueGrey,
-        body: const Center(
-          child: Text(
-            'hallo saya',
-            // maxLines: 2,
-            // overflow: TextOverflow.ellipsis,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              backgroundColor: Colors.deepPurple,
-              color: Colors.white,
-              fontSize: 20,
+        backgroundColor: Colors.white,
+        // body: const Center(
+        //   child: Text(
+        //     'hallo saya',
+        //     // maxLines: 2,
+        //     // overflow: TextOverflow.ellipsis,
+        //     textAlign: TextAlign.center,
+        //     style: TextStyle(
+        //       backgroundColor: Colors.deepPurple,
+        //       color: Colors.white,
+        //       fontSize: 20,
+        //     ),
+        //   ),
+        // ),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.redAccent,
             ),
-          ),
+            Container(
+              height: 102,
+              width: 102,
+              color: Colors.blueAccent,
+            ),
+            Container(
+              height: 105,
+              width: 105,
+              color: Colors.greenAccent,
+            ),
+          ],
         ),
       ),
     );
